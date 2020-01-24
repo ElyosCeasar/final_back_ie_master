488e6dc5ff48629a5e81a3889ae28b5ceb07629e
ElyosCaesar <ElyosCaesar@DESKTOP-7AUGHBB>
2020-01-25 01:26:37 +0330

    x

--- application/auth.js
+++ application/auth.js
@@ -22,28 +22,6 @@ const functions = {
         "your-256-bit-secret"
       );
     }
-  },
-  createUser(data) {
-    //it is for admin
-    console.log("user data: ", data);
-    const user = new model(data);
-    const userRole = authDataAccess.login(
-      user.username.trim(),
-      user.password.trim()
-    );
-    if (userRole === "notDef") {
-      return null;
-    } else {
-      const secondsSinceEpoch = Math.round(Date.now() / 1000);
-      return jwt.sign(
-        {
-          Role: userRole,
-          username: user.username.trim(),
-          exp: secondsSinceEpoch + 600
-        },
-        "your-256-bit-secret"
-      );
-    }
   }
 };
 
--- presentation/form.js
+++ presentation/form.js
@@ -9,6 +9,7 @@ const formHandler = require("../application/form");
 const authHandler = require("../application/auth");
 const adminHandler = require("../application/admin");
 const PORT = process.env.PORT || 5000;
+var jwt = require("jsonwebtoken");
 
 app.get("/", (req, res) => {
   res.send("Welcome to FormNegar app");
@@ -16,24 +17,56 @@ app.get("/", (req, res) => {
 
 //start $form reg
 
-app.get("/api/forms/:id", (req, res) => {
+app.get("/api/forms/:id", ensureToken, (req, res) => {
   try {
-    const { id } = req.params;
-    const { form, error } = formHandler.fetch(id);
-    if (error) {
-      res.error(error);
-    }
-    res.send({ form });
+    jwt.verify(req.token, "your-256-bit-secret", function(err, decoded) {
+      if (err) {
+        res.sendStatus(403);
+      } else {
+        if (
+          !(
+            "fieldAgent" === decoded.Role ||
+            "controlCentreAgent" === decoded.Role
+          )
+        ) {
+          res.status(400).send("شما به این بخش دسترسی ندارید");
+          return;
+        }
+        const { id } = req.params;
+        const { form, error } = formHandler.fetch(id);
+        if (error) {
+          res.error(error);
+        }
+        res.send({ form });
+      }
+    });
   } catch (error) {
     console.log(error);
     res.status(500).send("حطای پیشبینی نشده");
   }
 });
 
-app.get("/api/forms", (req, res) => {
+app.get("/api/forms", ensureToken, (req, res) => {
   try {
-    const forms = formHandler.fetchAllForms();
-    res.send({ forms });
+    jwt.verify(req.token, "your-256-bit-secret", function(err, decoded) {
+      if (err) {
+        res.sendStatus(403);
+      } else {
+        if (
+          !(
+            "fieldAgent" === decoded.Role ||
+            "controlCentreAgent" === decoded.Role
+          )
+        ) {
+          res.status(400).send("شما به این بخش دسترسی ندارید");
+          return;
+        }
+
+        const forms = formHandler.fetchAllForms();
+        console.log("f4", forms);
+        res.send({ forms });
+      }
+    });
   } catch (error) {
     console.log(error);
     res.status(500).send("حطای پیشبینی نشده");
@@ -41,20 +74,46 @@ app.get("/api/forms", (req, res) => {
 });
 app.post("/api/forms", (req, res) => {
   try {
+    // jwt.verify(req.token, "your-256-bit-secret", function(err, decoded) {
+    //   if (err) {
+    //     res.sendStatus(403);
+    //   } else {
+    //     if ("admin" !== decoded.Role) {
+    //       res.status(400).send("شما به این بخش دسترسی ندارید");
+    //      return;
+    //     }
     const form = req.body;
     formHandler.insert(form);
     res.send("form inserted");
+    //   }
+    // });
   } catch (error) {
     console.log(error);
     res.status(500).send("حطای پیشبینی نشده");
   }
 });
 
-app.post("/api/forms/:id", (req, res) => {
+app.post("/api/forms/:id", ensureToken, (req, res) => {
   try {
-    const form = req.body;
-    formHandler.printForm(form);
-    res.send({ form });
+    jwt.verify(req.token, "your-256-bit-secret", function(err, decoded) {
+      if (err) {
+        res.sendStatus(403);
+      } else {
+        // console.log(decoded.Role);
+        if (
+          !(
+            "fieldAgent" === decoded.Role ||
+            "controlCentreAgent" === decoded.Role
+          )
+        ) {
+          res.status(400).send("شما به این بخش دسترسی ندارید");
+          return;
+        }
+        const form = req.body;
+        formHandler.printForm(form);
+        res.send({ form });
+      }
+    });
   } catch (error) {
     console.log(error);
     res.status(500).send("حطای پیشبینی نشده");
@@ -85,22 +144,43 @@ app.post("/api/auth/login", (req, res) => {
 
 //end $auth
 //start $admin
-app.post("/api/admin/createUser", (req, res) => {
+app.post("/api/admin/createUser", ensureToken, (req, res) => {
   try {
-    const data = req.body;
-
-    const result = adminHandler.createUser(data);
-    if (result === true) {
-      res.status(200).send("کاربر ایجاد شد");
-    } else {
-      res.status(400).send("کاربر ایجاد نشد");
-    }
+    jwt.verify(req.token, "your-256-bit-secret", function(err, decoded) {
+      if (err) {
+        res.sendStatus(403);
+      } else {
+        if ("admin" !== decoded.Role) {
+          res.status(400).send("شما به این بخش دسترسی ندارید");
+          return;
+        }
+        const datas = req.body;
+        const result = adminHandler.createUser(datas);
+        if (result === true) {
+          res.status(200).send("کاربر ایجاد شد");
+        } else {
+          res.status(400).send("کاربر ایجاد نشد");
+        }
+      }
+    });
   } catch (error) {
     console.log(error);
     res.status(500).send("حطای پیشبینی نشده");
   }
 });
 
+//end $admin
+function ensureToken(req, res, next) {
+  const bearerheader = req.headers["authorization"];
+  if (typeof bearerheader !== "undefined") {
+    const bearer = bearerheader.split(" ");
+    const bearerToken = bearer[1];
+    req.token = bearerToken;
+    next();
+  } else {
+    res.status(403).send("توکن وجود ندارد");
+  }
+}
 app.listen(PORT, () => console.log("FormNegar app listening on port " + PORT));
 
 // http://localhost:5000/api/forms
